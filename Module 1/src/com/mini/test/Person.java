6b6574d852ac7a7e0b54e8600ca6f2b7b30e3e60
JDRFdev <2305juanda@gmail.com>
2026-04-22 21:19:05 -0500

    feat: Add Person.java

---
+++ Module 1/src/com/mini/test/Person.java
@@ -0,0 +1,37 @@
+package com.mini.test;
+
+public class Person {
+        private String name;
+        private int old;
+        private String email;
+
+    public Person(String name, int old, String email) {
+        this.name = name;
+        this.old = old;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOld(int old) {
+        this.old = old;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getOld() {
+        return old;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
