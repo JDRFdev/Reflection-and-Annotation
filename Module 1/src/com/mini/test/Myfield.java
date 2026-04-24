b0e829fcf085ee93a7d16fd7e42a875c156c7f13
JDRFdev <2305juanda@gmail.com>
2026-04-23 22:02:51 -0500

    feat: Add Myfield.Java

---
+++ Module 1/src/com/mini/test/Myfield.java
@@ -0,0 +1,13 @@
+package com.mini.test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Myfield {
+    String description();
+    boolean requiered () default false;
+}
