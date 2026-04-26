4cce2219b8f473c5145ca9afe1c11fcf45180e10
JDRFdev <2305juanda@gmail.com>
2026-04-25 21:45:15 -0500

    refactor: test in main

--- Module 2/src/Main.java
+++ Module 2/src/Main.java
@@ -1,6 +1,17 @@
+import com.JDRFdev.Chef;
+import com.JDRFdev.Container;
+import com.JDRFdev.Oven;
+import com.JDRFdev.Restaurant;
+
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-
+        Set<Class<?>> cls=Set.of(Restaurant.class, Chef.class, Oven.class);
+        Container c=new Container(cls);
+        Restaurant r= c.getObject(Restaurant.class);
+        r.getChef().setName("Juan");
+        r.serv();
+        System.out.println(r.getChef().getName());
     }
 }
